f6c7fec59d2ec2abfb4eb19aae19e06df5dba2c2
narutoxmani <narutosworld007@gmail.com>
2025-02-15 23:02:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,11 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+if CHANNEL_ID.startswith("-100"):
+    CHANNEL_ID = int(CHANNEL_ID)
+else:
+    raise ValueError("Invalid CHANNEL_ID! Make sure it starts with -100.")
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
